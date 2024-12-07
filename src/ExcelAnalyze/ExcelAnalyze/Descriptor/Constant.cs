38846c010b705243e44b4add4aae72f649fa7354
Dmytro Trofimchuk <53020216+trofim0904@users.noreply.github.com>
2024-12-07 11:26:49 +0200

    Feature-4 Add XLSB format (#6)
    Co-authored-by: Dmytro Trofimchuk <dmytro.trofimchuk@acupower.co.uk>

--- src/ExcelAnalyze/ExcelAnalyze/Descriptor/Constant.cs
+++ src/ExcelAnalyze/ExcelAnalyze/Descriptor/Constant.cs
@@ -2,6 +2,8 @@ namespace ExcelAnalyze.Descriptor
 {
     public static class Constant
     {
+        public const string FileSearchTitle = "Select an Excel File";
+        public const string FileSearchFilter = "Excel Files|*.xls;*.xlsx;*.xlsm;*.xlsb";
         public const int FullPercent = 100;
     }
 }
